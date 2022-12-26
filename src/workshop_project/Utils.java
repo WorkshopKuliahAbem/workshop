24bd0c04e28106e166961a7b7f8f04c6b1141f40
LuciferLovesMe <abhiemsmile@gmail.com>
2022-12-26 10:20:06 +0700

    add fitur saldo

--- src/workshop_project/Utils.java
+++ src/workshop_project/Utils.java
@@ -9,6 +9,9 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.*;
+import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.Locale;
 
 /**
  *
@@ -33,5 +36,52 @@ public class Utils {
         Connection conn = (Connection) foderoDB();
         PreparedStatement stm = conn.prepareStatement(query);
         stm.execute();
+    }
+    
+    public static String getSaldo() throws SQLException{
+        try{
+            String query, query1;
+            Statement stm, stm1;
+            ResultSet rs, rs1;
+            int saldo;
+            ArrayList<Integer> pendapatan = new ArrayList<Integer>();
+            ArrayList<Integer> pengeluaran = new ArrayList<Integer>();
+
+            query = "select * from pendapatan";
+            stm = (Statement) foderoDB().createStatement();
+            rs = stm.executeQuery(query);
+            while(rs.next()){
+                pendapatan.add(Integer.valueOf(rs.getString("jumlah_pendapatan")));
+            }
+            query = "select * from pengeluaran";
+            stm = (Statement) foderoDB().createStatement();
+            rs = stm.executeQuery(query);
+            while(rs.next()){
+                pengeluaran.add(Integer.valueOf(rs.getString("jumlah_pengeluaran")));
+            }
+
+            int pend = 0;
+            for(int i = 0; i < pendapatan.size(); i++){
+                pend += pendapatan.get(i);
+            }
+            int peng = 0;
+            for(int i = 0; i < pengeluaran.size(); i++){
+                peng += pengeluaran.get(i);
+                System.out.println(pengeluaran.get(i));
+            }
+            System.out.println(peng);
+
+            saldo = pend - peng;
+            double a = Double.valueOf(String.valueOf(saldo));
+            Locale localeID = new Locale("in", "ID");
+            NumberFormat formatRupiah = NumberFormat.getCurrencyInstance(localeID);
+
+            //merubah mendari 10.000.000.000
+            NumberFormat nf = NumberFormat.getNumberInstance(new Locale("in", "ID"));
+
+            return String.valueOf(nf.format(a));
+        } catch(Exception e){
+            throw e;
+        }
     }
 }
